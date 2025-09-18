e76b013dfb8da6cc5caa6b4f0426b0e3f82e8408
vanilao <kevinandst@gmail.com>
2025-09-18 18:07:58 +0700

    starting point

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",           // for Vite
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
